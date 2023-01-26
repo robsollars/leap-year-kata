4ce4ca02afcd1bf1dab685d1604677a50ba3332f
Rob Sollars <rob.sollars@thoughtworks.com>
2023-01-26 11:50:55 +0100

    adds the rules for the kata

--- src/LeapYear.test.ts
+++ src/LeapYear.test.ts
@@ -1,4 +1,20 @@
-import {LeapYear} from "./LeapYear";
+import {LeapYear} from './LeapYear';
+
+/**
+ *
+ * # Rules:
+ *
+ * - A year is leap if it is divisible by 4
+ * - Unless it is divisible by 100, then it’s not a leap year
+ * - Unless it is divisible by 400, then it’s a leap year
+ *
+ * # Tests:
+ *
+ * - [ ] 2022 is not leap
+ * - [ ] 2024 is leap
+ * - [ ] 1900 is not leap
+ * - [ ] 2000 is leap
+ */
 
 describe('Determining leap years', () => {
     it('works', () => {
